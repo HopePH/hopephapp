e63bb9f03a3f394626d376deb8f398f86de76325
Reynold <Reynald.Monares.Haiyan@gmail.com>
2018-07-09 15:13:16 +0800

    Implemented Contact Services and Post Feed Service for open source

--- Yol.Punla/Yol.Punla/GatewayAccess/Implementation/ContactService.cs
+++ Yol.Punla/Yol.Punla/GatewayAccess/Implementation/ContactService.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Diagnostics;
-using System.Net.Http;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Yol.Punla.AttributeBase;
-using Yol.Punla.Barrack;
 using Yol.Punla.Entity;
 using Yol.Punla.Mapper;
 
@@ -12,61 +9,37 @@ namespace Yol.Punla.GatewayAccess
     [DefaultModuleInterfaced(ParentInterface = typeof(IContactService))]
     public class ContactService : GatewayServiceBase, IContactService
     {
-        private readonly string _getUserByUsername = null;
-        private const string GETUSERBYUSERNAME = "Receiver/GetReceiverContactByUserName?userName={0}&facebookId={1}&companyName={2}";
-
-        private readonly string _postingReceiver = null;
-        private const string POSTINGRECEIVER = "Receiver/SavingReceiverDetails";
-
-        private readonly string _postVerifyEmail = null;
-        private const string POSTVERIFYEMAIL = "Receiver/VerifyEmail?toEmailAddress={0}&aliasName={1}";
-
-        private readonly string _getUserViaEmail = null;
-        private const string GETUSERVIAEMAIL = "Receiver/GetUserViaEmail?emailAddress={0}&companyName={1}";
-
         public ContactService(IServiceMapper serviceMapper) : base(serviceMapper)
         {
-            _getUserByUsername = BaseAPI + GETUSERBYUSERNAME;
-            _postingReceiver = BaseAPI + POSTINGRECEIVER;
-            _postVerifyEmail = BaseAPI + POSTVERIFYEMAIL;
-            _getUserViaEmail = BaseAPI + GETUSERVIAEMAIL;
+           
         }
 
-        public async Task<Entity.Contact> GetUserProfile(string EmailAdd, string FbId)
+        public async Task<Contact> GetUserProfile(string EmailAdd, string FbId)
         {
-            if (string.IsNullOrEmpty(EmailAdd.Trim()))
-                EmailAdd = "xyz";
+            await Task.Delay(1);
+            if (string.IsNullOrEmpty(FbId))
+                FbId = "zz0876";
 
-            var remoteItems = await GetRemoteAsync<Contract.ContactK>(string.Format(_getUserByUsername,EmailAdd,FbId, CompanyName));
-            return ServiceMapper.Instance.Map<Entity.Contact>(remoteItems);
+            return FakeData.FakeUsers.Contacts.Where(c => c.EmailAdd == EmailAdd || c.FBId == FbId).FirstOrDefault();
         }
 
-        public async Task<int> PostReceiver(Entity.Contact receiver)
+        public async Task<int> PostReceiver(Contact receiver)
         {
-            Debug.WriteLine("HOPEPH Posting the receiver.");
-            var contractItem = ServiceMapper.Instance.Map<Contract.ContactK>(receiver);
-            contractItem.TypeCode = "Receiver";
-            contractItem.EmailAddress = receiver.EmailAdd;
-            contractItem.DateModified = DateTime.Now.ToString(Constants.DateTimeFormat);
-            contractItem.UserName = receiver.EmailAdd;
-            contractItem.PhotoURL = receiver.PhotoURL ?? receiver.FBLink;   // if photoURL has no value, set it equal to fblink
-            contractItem.FBLink = receiver.FBLink;
-            contractItem.CompanyName = CompanyName;
-
-            var result = await PostRemoteAsync<int>(_postingReceiver, CastToStringContent<Contract.ContactK>(contractItem));
-            return result;
+            //chito. let's assume that the newly inserted contact has an ID of 1000
+            await Task.Delay(1);
+            return 1000;
         }
 
         public async Task<string> PostVerificationCode(string emailAddress)
         {
-            string endPoint = string.Format(_postVerifyEmail, emailAddress, "na");
-            return await PostRemoteAsync<string>(endPoint, new StringContent(""));
+            await Task.Delay(1);
+            return "1111";
         }
 
         public async Task<Contact> GetUserViaEmail(string emailAddress, string companyName)
         {
-            string endPoint = string.Format(_getUserViaEmail, emailAddress, companyName);
-            return await GetRemoteAsync<Contact>(endPoint);
+            await Task.Delay(1);
+            return FakeData.FakeUsers.Contacts.Where(c => c.EmailAdd == emailAddress).FirstOrDefault();
         }
     }
 }
--- Yol.Punla/Yol.Punla/GatewayAccess/Implementation/PostFeedService.cs
+++ Yol.Punla/Yol.Punla/GatewayAccess/Implementation/PostFeedService.cs
@@ -1,9 +1,6 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Yol.Punla.AttributeBase;
 using Yol.Punla.Mapper;
@@ -13,133 +10,68 @@ namespace Yol.Punla.GatewayAccess
     [DefaultModuleInterfaced(ParentInterface = typeof(IPostFeedService))]
     public class PostFeedService : GatewayServiceBase, IPostFeedService
     {
-        private readonly string _getTopPostFeeds = null;
-        private const string GETTOPPOSTFEEDS = "PostFeed/GetTopPosts?companyName={0}&contactId={1}";
-
-        private readonly string _getPostFeedComments = null;
-        private const string GETPOSTFEEDCOMMENTS = "PostFeed/GetTopComments?parentPostFeedId={0}&companyName={1}&contactId={2}";
-
-        private readonly string _postNewPostFeed = null;
-        private const string NEWPOSTFEED = "PostFeed/AddNewPostFeed";
-
-        private readonly string _postUpdatePostFeed = null;
-        private const string UPDATEPOSTFEED = "PostFeed/UpdatePostFeed";
-
-        private readonly string _deleteSelfPost = null;
-        private const string DELETESELFPOST = "PostFeed/DeletePostFeed?postFeedId={0}&companyName=Hopeph";
-
-        private readonly string _supportPost = null;
-        private const string SUPPORTPOST = "PostFeed/LikeAPost?postFeedId={0}&contactId={1}";
-
-        private readonly string _unsupportPost = null;
-        private const string UNSUPPORTPOST = "PostFeed/UnLikeAPost?postFeedId={0}&contactId={1}";
-
-        private readonly string _getOwnPosts = null;
-        private const string GETOWNPOSTS = "PostFeed/GetOwnPosts?companyName={0}&contactId={1}";
-
-        private readonly string _getPostsNotifications = null;
-        private const string GETPOSTSNOTIFICATIONS = "PostFeed/GetPostNotifications?companyName={0}&contactId={1}";
-
-        private readonly string _getPostsWithSpeed = null;
-        private const string GETPOSTSWITHSPEED = "PostFeed/GetTopPostsWithSpeed?contactId={0}&isFirstLoad={1}&topPostId={2}&companyName={3}";
-
         public PostFeedService(IServiceMapper serviceMapper) : base(serviceMapper)
         {
-            _getTopPostFeeds = BaseAPI + GETTOPPOSTFEEDS;
-            _getPostFeedComments = BaseAPI + GETPOSTFEEDCOMMENTS;
-            _postNewPostFeed = BaseAPI + NEWPOSTFEED;
-            _postUpdatePostFeed = BaseAPI + UPDATEPOSTFEED;
-            _deleteSelfPost = BaseAPI + DELETESELFPOST;
-            _supportPost = BaseAPI + SUPPORTPOST;
-            _unsupportPost = BaseAPI + UNSUPPORTPOST;
-            _getOwnPosts = BaseAPI + GETOWNPOSTS;
-            _getPostsNotifications = BaseAPI + GETPOSTSNOTIFICATIONS;
-            _getPostsWithSpeed = BaseAPI + GETPOSTSWITHSPEED;
+          
         }
         
         public async Task<IEnumerable<Entity.PostFeed>> GetTopPostFeeds(int posterId)
         {
-            var remoteItems = await GetRemoteAsync<IEnumerable<Contract.PostFeedK>>(string.Format(_getTopPostFeeds, CompanyName, posterId));
-            var mappedItems = ServiceMapper.Instance.Map<IEnumerable<Entity.PostFeed>>(remoteItems);
-            return mappedItems;
+            await Task.Delay(1);
+            var topPostFeeds = FakeData.FakePostFeeds.Posts.Where(p => p.PosterId == posterId);
+            return topPostFeeds;
         }
 
         public async Task<IEnumerable<Entity.PostFeed>> GetPostFeedsWithSpeed(int posterId, int postFeedBaseId, bool isFirstLoad)
         {
-            var endPoint = string.Format(_getPostsWithSpeed, posterId, isFirstLoad, postFeedBaseId, CompanyName);
-            var remoteItems = await GetRemoteAsync<IEnumerable<Contract.PostFeedK>>(endPoint);
-            var mappedItems = ServiceMapper.Instance.Map<IEnumerable<Entity.PostFeed>>(remoteItems);
-            return mappedItems;
+            await Task.Delay(1);
+            var postFeeds = FakeData.FakePostFeeds.Posts;
+            return postFeeds;
         }
 
         public async Task<IEnumerable<Entity.PostFeed>> GetPostFeedComments(int postFeedId, int posterId)
         {
-            var remoteItems = await GetRemoteAsync<IEnumerable<Contract.PostFeedK>>(string.Format(_getPostFeedComments, postFeedId, CompanyName, posterId));
-            var mappedItems = ServiceMapper.Instance.Map<IEnumerable<Entity.PostFeed>>(remoteItems);
-            return mappedItems;
+            await Task.Delay(1);
+            var postFeedComments = FakeData.FakePostFeeds.Posts.Where(p => p.PostFeedID == postFeedId && p.PosterId == posterId);
+            return postFeedComments;
         }
 
         public async Task<HttpStatusCode> DeleteSelfPostFromRemote(int postId)
         {
-            Debug.WriteLine("HOPEPH Posting new post feed.");
-            var jsonContent = new StringContent("", Encoding.UTF8, "application/json");
-            var result = await HttpClient.PostAsync(string.Format(_deleteSelfPost, postId), jsonContent);
-            return result.StatusCode;
+            await Task.Delay(1);
+            return HttpStatusCode.OK;
         }
 
         public async Task<HttpStatusCode> SupportPost(int postFeedId, int posterId)
         {
-            Debug.WriteLine("HOPEPH Supporting a post feed.");
-            var jsonContent = new StringContent("", Encoding.UTF8, "application/json");
-            var result = await HttpClient.PostAsync(string.Format(_supportPost, postFeedId, posterId), jsonContent);
-            return result.StatusCode;
+            await Task.Delay(1);
+            return HttpStatusCode.OK;
         }
 
         public async Task<HttpStatusCode> UnSupportPost(int postFeedId, int posterId)
         {
-            Debug.WriteLine("HOPEPH Supporting a post feed.");
-            var jsonContent = new StringContent("", Encoding.UTF8, "application/json");
-            var result = await HttpClient.PostAsync(string.Format(_unsupportPost, postFeedId, posterId), jsonContent);
-            return result.StatusCode;
+            await Task.Delay(1);
+            return HttpStatusCode.OK;
         }
 
         public async Task<IEnumerable<Entity.PostFeed>> GetOwnPostsFeeds(int posterId)
         {
-            var remoteItems = await GetRemoteAsync<IEnumerable<Contract.PostFeedK>>(string.Format(_getOwnPosts, CompanyName, posterId));
-            var mappedItems = ServiceMapper.Instance.Map<IEnumerable<Entity.PostFeed>>(remoteItems);
-            return mappedItems;
+            await Task.Delay(1);
+            var postFeeds = FakeData.FakePostFeeds.Posts.Where(p => p.PosterId == posterId);
+            return postFeeds;
         }
 
         public async Task<IEnumerable<Entity.PostFeed>> GetPostFeedNotifications(int posterId)
         {
-            var remoteItems = await GetRemoteAsync<IEnumerable<Contract.PostFeedK>>(string.Format(_getPostsNotifications, CompanyName, posterId));
-            var mappedItems = ServiceMapper.Instance.Map<IEnumerable<Entity.PostFeed>>(remoteItems);
-            return mappedItems;
+            await Task.Delay(1);
+            var postFeeds = FakeData.FakePostFeeds.Posts.Where(p => p.PosterId == posterId);
+            return postFeeds;
         }
 
         public async Task<int> SavePostToServer(Entity.PostFeed newPost)
         {
-            int newPostId = 0;
-
-            Debug.WriteLine("HOPEPH Posting new post feed.");
-            var contractItem = ServiceMapper.Instance.Map<Contract.PostFeedK>(newPost);
-
-            var jsonString = JsonConvert.SerializeObject(contractItem);
-            var jsonContent = new StringContent(jsonString, Encoding.UTF8, "application/json");
-            var result = await HttpClient.PostAsync(_postNewPostFeed, jsonContent);
-
-            if (result.IsSuccessStatusCode)
-            {
-                var jsonResult = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-                if (!string.IsNullOrWhiteSpace(jsonResult))
-                {
-                    newPostId = JsonConvert.DeserializeObject<int>(jsonResult);
-                }
-
-            }
-
-            return newPostId;
+            await Task.Delay(1);
+            return 0;
         }
     }
 }
