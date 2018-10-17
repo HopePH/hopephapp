89b18da6f36b5d756a39f8f170956168e836c447
alfie <alfiesalano02@gmail.com>
2018-10-17 11:06:28 +0800

    Polish PostFeedPage part 1
    Polish PostFeedPage part 1

--- Yol.Punla/Yol.Punla.Droid/CustomRenderers/CustomEntryRenderer.cs
+++ Yol.Punla/Yol.Punla.Droid/CustomRenderers/CustomEntryRenderer.cs
@@ -11,6 +11,10 @@ namespace Yol.Punla.Droid.CustomRenderers
 {
     public class CustomEntryRenderer : EntryRenderer
     {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+
         protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
         {
             base.OnElementChanged(e);
