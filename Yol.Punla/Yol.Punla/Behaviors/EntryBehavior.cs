bad83a224230145ca43fad001abfab4dff039c76
alfie <alfiesalano02@gmail.com>
2019-02-27 17:23:26 +0800

    Implemented changing bottomline color of entry when focus state is changed

---
+++ Yol.Punla/Yol.Punla/Behaviors/EntryBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Xamarin.Forms;
+
+namespace Yol.Punla.Behaviors
+{
+    public class EntryBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.Focused += Bindable_Focused;
+        }
+
+        private void Bindable_Focused(object sender, FocusEventArgs e)
+        {
+            var entry = sender as Entry;
+            var effect = (Effects.EntryEffect)entry.Effects.FirstOrDefault(ef => ef is Effects.EntryEffect);
+
+            if(effect != null)
+            {
+                effect.LineColor = Color.Red;
+            }
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
---
+++ Yol.Punla/Yol.Punla/Effects/EntryEffectParameters.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Xamarin.Forms;
+
+namespace Yol.Punla.Effects
+{
+    public static class EntryEffectParameters
+    {
+        private const string GREEN = "#FF417505";
+        private const string GRAY = "#1F000000";
+
+        public static readonly BindableProperty IsFocusedProperty = BindableProperty.CreateAttached("IsFocused", typeof(bool), typeof(EntryEffectParameters), (bool)false, BindingMode.TwoWay, null, propertyChanged: OnFocusStateChanged);
+
+        public static void OnFocusStateChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var view = bindable as Entry;
+            if (view == null) return;
+
+            var effect = (EntryEffect)view.Effects.FirstOrDefault(e => e is EntryEffect);
+            
+            if (effect != null)
+            {
+                var newEffect = new EntryEffect
+                {
+                    LineColor = Color.Red,
+                    NumberOfLines = effect.NumberOfLines,
+                    PlaceholderToLineDistance = effect.PlaceholderToLineDistance,
+                    Thickness = effect.Thickness
+                };
+
+                if (view.IsFocused) newEffect.LineColor = Color.FromHex(GREEN);
+                else newEffect.LineColor = Color.FromHex(GRAY);
+                
+                view.Effects.Remove(effect);
+                view.Effects.Add(newEffect);
+            }
+        }
+    }
+}
