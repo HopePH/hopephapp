543bb3eeb0e4e7eb3925a99135a96746008e54fc
REY <reynald.monares.haiyan@gmail.com>
2019-04-09 15:02:49 +0800

    started adding questionare page

---
+++ Yol.Punla/Yol.Punla/Views/QuestionarePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Yol.Punla.AttributeBase;
+
+namespace Yol.Punla.Views
+{
+    [ModuleView]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuestionarePage : ContentPage
+    {
+        public QuestionarePage()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (XamlParseException xp)
+            {
+                if (!xp.Message.Contains("StaticResource not found for key"))
+                    throw;
+            }
+            catch (Exception ex)
+            {
+                if (!(ex.Source == "FFImageLoading.Forms" || ex.Source == "FFImageLoading.Transformations"))
+                    throw;
+            }
+        }
+    }
+}
